1b2250904f25a8b06b00ca67aa5338d40db197ac
Mo Battah <mohhandb@hotmail.com>
2020-04-19 21:40:14 -0500

    Updated prompts

--- LinuxSecurityGroupChk.py
+++ LinuxSecurityGroupChk.py
@@ -8,11 +8,11 @@ import sys
 
 
 key = paramiko.RSAKey(data=base64.b64decode(b'AAAAB3NzaC1yc2EAAAADAQABAAAAgQC37NsG62lBjOkOIB63EpK3R0ZPM4ZOPmvRlt57OBgzysA6zkbS8u/Q4r87bzdwwFiXzMlBwzvJJ0oAmFqVzfMVamuPCVN7tc1Ur/sDFsP7qbrRvQTF5MuXnHP3QSC4DwN6UAMExvdfw5OgwbpGe1KS3iMKk53W2YTLQIEUW2+AsQ=='))
-user = str(input("What is your CaryNT username?"))
-passwd = str(input("What is your CaryNT pass?"))
+user = str(input("What is your username?"))
+passwd = str(input("What is your pass?"))
 hostconnect = str(input("What host are you trying to connect to ?"))
 client = paramiko.SSHClient()
-client.get_host_keys().add('intsa245au.vsp.sas.com', 'ssh-rsa', key)
+client.get_host_keys().add('yourserver.vsp.sas.com', 'ssh-rsa', key)
 client.connect(hostconnect, username=user, password=passwd)
 stdin, stdout, stderr = client.exec_command('ls')
 for line in stdout:
